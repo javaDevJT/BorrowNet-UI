538550982304d10df48067b5a848100ed2565d64
riccardo-morelli <riccardo.morelli@edu.unife.it>
2024-09-29 01:14:05 +0200

    added components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Hello from './components/Hello'
 
 const App = () => {
   return (
-    <h1 className="text-red-500">Hello World!</h1>
+    <Hello name="Marta and Riccardo"/>
   )
 }
 
