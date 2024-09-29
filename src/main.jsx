d0c89f836becaa8e69d685bdeee50b8372e6b11d
riccardo-morelli <riccardo.morelli@edu.unife.it>
2024-09-29 20:48:51 +0200

    configured Material UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,31 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Default font for Material UI
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0052cc',
+    },
+    secondary: {
+      main: '#edf2ff',
+    },
+  },
+});
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
--- src/pages/TestingCSSPage.jsx
+++ src/pages/TestingCSSPage.jsx
@@ -1,11 +1,46 @@
+import { Box, Button, Container, Paper, Typography } from '@mui/material';
 import React from 'react'
+
+const reviewsList = ["John", "Emily", "Mike", "Henry"];
+const reviewContent = `Niente è paragonabile. Esiste forse cosa
+    che non sia tutta sola con sé stessa e indicibile?
+    Invano diamo nomi, solo è dato accettare
+    e accordarci che forse qua un lampo, là uno sguardo
+    ci abbia sfiorato, come
+    se proprio in questo consistesse vivere
+    la nostra vita.`
 
 const TestingCSSPage = () => {
   return (
-    <div>
-        <h1>Hello</h1>
-        <p>Hello</p>
-    </div>
+    <Container>
+        <Typography
+         variant="h2"
+         sx={{my: 4, textAlign: "center", color: "primary.main"}}
+        >
+            Reviews
+        </Typography>
+        <Box
+          sx={{
+            pt: 4,
+            display: "flex",
+            justifyContent: "space-between",
+            gap: 4,
+            flexDirection: "column",
+
+          }}
+        >
+            {reviewsList.map((review) => (
+                <Paper elevation={5}>
+                    <Box sx={{m:3}}>
+                        <Typography variant='h4'>{review}</Typography>
+                        <Typography sx={{mt:2}}>{reviewContent}</Typography>
+                        <Button sx={{m:1}}>Hello</Button>
+                    </Box>
+                </Paper>
+            ))}
+        </Box>
+
+    </Container>
 
   )
 }
