4d01e5d551041348be28870b703908ce638fc5c3
Marta Malagutti <marta01.malagutti@edu.unife.it>
2024-09-28 19:08:05 -0500

    router, main layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,29 @@ import {
 } from 'react-router-dom';
 import React from 'react'
 import Hello from './components/Hello'
+import RegistrationPage from './pages/RegistrationPage';
+import LoginPage from './pages/LoginPage';
+import NewProfilePage from './pages/NewProfilePage';
+import EditProfilePage from './pages/EditProfilePage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
 
 
 const App = () => {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/registration' element={<RegistrationPage />} />
+        <Route path='/new-profile' element={<NewProfilePage />} />
+        <Route path='/profile/:id' element={<ProfilePage />} />
+        <Route path='/edit-profile/:id' element={<EditProfilePage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
   return (
     <>
       <Hello name="Marta and Riccardo"/>
