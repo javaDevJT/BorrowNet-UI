1a7f1531dfceddc45a9641b0948ad49566e4bea2
riccardo-morelli <riccardo.morelli@edu.unife.it>
2024-10-05 09:26:10 +0200

    added navbar and drawer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,91 @@
-import { Container } from '@mui/material'
+import { AppBar, Box, Container, Divider, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+import MailIcon from '@mui/icons-material/Mail';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+
+
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 
+
 const MainLayout = () => {
+
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        <ListItem key='1' disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary='Filler' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key='2' disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary='Filler' />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key='3' disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <MailIcon />
+            </ListItemIcon>
+            <ListItemText primary='Filler' />
+          </ListItemButton>
+        </ListItem>
+        <Divider />
+        <ListItem key='4' disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText primary='Log out' />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
   return (
-    <Container
-    sx={{ bgcolor: "secondary.main" , padding: 2 }}
-    >
-      <Outlet/>
-    </Container>
+    <React.Fragment>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              aria-label="menu"
+              sx={{ mr: 2, color: 'white' }}
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon/>
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              BorrowNet
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer open={open} onClose={toggleDrawer(false)}>
+        {DrawerList}
+      </Drawer>
+      
+      <Container sx={{ bgcolor: "secondary.main" , padding: 2 }}>
+        <Outlet/>
+      </Container>
+    </React.Fragment>
+
   )
 }
 
