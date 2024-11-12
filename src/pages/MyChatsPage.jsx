5f56c5cf3b38944c3505c4c5614bb933bea38114
Mororororo <riccardomorelli2001@gmail.com>
2024-11-12 12:33:44 -0600

    GET chatList

--- src/pages/MyChatsPage.jsx
+++ src/pages/MyChatsPage.jsx
@@ -1,10 +1,38 @@
-import { Card, Paper, Stack, Typography } from '@mui/material'
-import React from 'react'
+import { Paper, Stack, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import useAuthHeader from 'react-auth-kit/hooks/useAuthHeader';
 import { useNavigate } from 'react-router-dom';
 
 const MyChatsPage = () => {
     let chatList = [1, 2, 3];
     const navigate = useNavigate();
+    const authHeader = useAuthHeader();
+    
+    const [showAlert, setShowAlert] = useState(false);
+    const [chatListTest, setUserList] = useState([]);
+
+    useEffect(() => {
+    fetch(`/api/chat`, {
+      method: 'GET',
+      headers: {
+        'Authorization': authHeader,
+        'Content-Type': 'application/json'
+      }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response error.');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUserList(data.content);
+      })
+      .catch((error) => {
+        console.error(error);
+        setShowAlert(true);
+      });
+    }, []);
 
     return (
         <Stack>
