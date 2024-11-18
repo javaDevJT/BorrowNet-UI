acf63b1467f4c34961819577a5d3aea1ad2c95f7
Mororororo <riccardomorelli2001@gmail.com>
2024-11-18 13:10:11 -0600

    added lender data to the post details page

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Box, Typography, Avatar, TextField, Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { Box, Typography, Avatar, TextField, Button, CircularProgress, Paper } from '@mui/material';
 import useAuthHeader from "react-auth-kit/hooks/useAuthHeader";
 import { useNavigate } from 'react-router-dom';
+import useFetchUserData from '../components/useFetchUserData';
 
 const DetailsPage = () => {
   const location = useLocation();
@@ -19,6 +20,16 @@ const DetailsPage = () => {
       setRentalDays(post.maxRentalDays);
     }
   };
+
+  const { userData, loading, error } = useFetchUserData(post.lender, authHeader);
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  if (error) {
+    return <p>Error loading user data: {error.message}</p>;
+  }
 
   const handleBorrow = async () => {
     const navigate = useNavigate();
@@ -52,25 +63,44 @@ const DetailsPage = () => {
 
   return (
     <Box sx={{ p: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Typography variant="h4" gutterBottom>
+      <Typography variant="h4" gutterBottom sx={{marginBottom: 4}}>
         {post.itemName}
       </Typography>
-      <Avatar
-        variant="square"
-        src={post.itemPhoto ? `data:image/JPG;base64,${post.itemPhoto}` : 'src/assets/n-a-512.png'}
-        alt={post.itemName}
-        sx={{ width: 200, height: 200, mb: 2 }}
-      />
-      <Typography variant="body1" gutterBottom>
-        {post.itemDescription || 'Description not available.'}
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        Available for a maximum of {post.maxRentalDays} days
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        Condition of the item: {post.condition}
-      </Typography>
-      <Box sx={{ mt: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', maxWidth: 400 }}>
+
+      <Paper sx={{display: 'flex', flexDirection: 'row', p:4, borderRadius: 4}}>
+        <Avatar
+          variant="square"
+          src={post.itemPhoto ? `data:image/JPG;base64,${post.itemPhoto}` : 'src/assets/n-a-512.png'}
+          alt={post.itemName}
+          sx={{ width: 200, height: 200, mb: 2, borderRadius: 4, marginRight: 3, marginLeft: 1 }}
+        />
+        <Box>
+
+          <Typography variant='h6'>Details</Typography>
+          <Typography variant="body1" gutterBottom>
+          {post.itemDescription || ''}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Available for a maximum of {post.maxRentalDays} days
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            Condition of the item: {post.condition}
+          </Typography>
+          <Typography variant='h6'>Lender</Typography>
+          <Typography
+            variant="body1"
+            component={Link}
+            to={`/profile/${userData.id}`}
+            sx={{ textDecoration: 'underline', color: 'inherit', cursor: 'pointer' }}          >
+            {userData.firstName + ' ' + userData.lastName}
+          </Typography>          
+        </Box>
+
+
+      </Paper>
+      
+      
+      <Box sx={{ mt: 4, display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%', maxWidth: 530 }}>
         <TextField
           label={`Choose a number of days`}
           type="number"
