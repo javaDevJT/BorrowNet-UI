fee52fc5c6052a64bf26a33e58fc9a30aa2ba26d
Mororororo <riccardomorelli2001@gmail.com>
2024-11-20 03:54:11 -0600

    reverted theme changes

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -11,29 +11,18 @@ export const useTheme = () => {
 const CustomThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
 
-  // Define separate palettes for light and dark modes
-  const getDesignTokens = (mode) => ({
+  // Create Material UI theme based on current theme state
+  const muiTheme = createTheme({
     palette: {
-      mode,
+      mode: theme,
       primary: {
         main: '#0052cc',
       },
       secondary: {
-        main: mode === 'light' ? '#edf2ff' : '#666363',
-      },
-      background: {
-        default: mode === 'light' ? '#ffffff' : '#121212',
-        paper: mode === 'light' ? '#f4f6f8' : '#1e1e1e',
-      },
-      text: {
-        primary: mode === 'light' ? '#000000' : '#ffffff',
-        secondary: mode === 'light' ? '#555555' : '#ffffff', 
+        main: '#edf2ff',
       },
     },
   });
-
-  // Create Material UI theme based on current theme state
-  const muiTheme = createTheme(getDesignTokens(theme));
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
